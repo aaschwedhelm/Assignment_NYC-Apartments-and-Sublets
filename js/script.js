e6be3425f4893e3b4064159b25ea7e791b699175
Alejandro <aaschwedhelm@users.noreply.github.com>
2017-01-08 14:35:53 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 var map = L.map('map').setView([40.68025, -74.00155], 13);
 	
-L.tileLayer('https://a.tiles.mapbox.com/v3/mapbox.world-bright/{z}/{x}/{y}.png', {
+L.tileLayer('http://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', {
 	minZoom: 0,
 	maxZoom: 18,
-	
-}).addTo(map);
+	attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
 
-var credits = L.control.attribution().addTo(map);
-credits.addAttribution('© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>');
+}).addTo(map);
 
 function popUp(feature, layer) {
   layer.bindPopup(feature.properties.Neighborhood + '<br/>' + feature.properties.Start_date + '<br/>' + feature.properties.End_date);
